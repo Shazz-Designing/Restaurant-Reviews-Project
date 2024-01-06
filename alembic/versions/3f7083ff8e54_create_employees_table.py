3f6258df68bed01ef800a2397ea40c1f9aeecf46
sharon <sapot@safaricom.co.ke>
2024-01-06 20:29:06 +0300

    set up the requirements

---
+++ alembic/versions/3f7083ff8e54_create_employees_table.py
@@ -0,0 +1,9 @@
+# Create 'employees' table
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
---
+++ seeds.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Restaurant, Customer, Review
+
+engine = create_engine('sqlite:///restaurant_reviews.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+restaurant1 = Restaurant(name='Java', price=3)
+restaurant2 = Restaurant(name='Art Cafe', price=2)
+
+customer1 = Customer(first_name='John', last_name='Doe')
+customer2 = Customer(first_name='Jane', last_name='Doe')
+
+review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
+review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer2)
+
+session.add_all([restaurant1, restaurant2, customer1, customer2, review1, review2])
+session.commit()
